bf743cea1609d976d1f569bc9ea7d88a6dfeba1c
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-03-09 17:04:43 -0500

    update cffconvert

--- stubs/cffconvert/cli/cli.pyi
+++ stubs/cffconvert/cli/cli.pyi
@@ -1,8 +1,9 @@
-from _typeshed import Incomplete
+from typing import Any
 from cffconvert.cli.check_early_exits import check_early_exits as check_early_exits
 from cffconvert.cli.create_citation import create_citation as create_citation
 from cffconvert.cli.validate_or_write_output import validate_or_write_output as validate_or_write_output
 
-options: Incomplete
+options: dict[str, dict[str, Any]]
+epilog: str
 
-def cli(infile, outfile, outputformat, url, show_help, show_trace, validate_only, version) -> None: ...
+def cli( infile: str | None, outfile: str | None, outputformat: str | None, url: str | None, show_help: bool, show_trace: bool, validate_only: bool, version: bool, verbose: bool ) -> None: ...
