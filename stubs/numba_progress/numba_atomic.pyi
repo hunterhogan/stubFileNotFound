fa878e748778e6d6a176f80e1a0df565f4a87769
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-03-16 14:33:05 -0500

    update numba_progress

--- stubs/numba_progress/numba_atomic.pyi
+++ stubs/numba_progress/numba_atomic.pyi
@@ -1,37 +1,54 @@
+from numpy.typing import NDArray
+from typing import Any, Union, TypeVar
+import numpy as np
+
+T = TypeVar('T', bound=np.number[Any])
+
 __all__ = ['atomic_add', 'atomic_sub', 'atomic_max', 'atomic_min', 'atomic_xchg']
 
-def atomic_add(ary, i, v):
+def atomic_add(ary: NDArray[T], i: Union[int, tuple[int,...]], v: T) -> T:
     """
-    Atomically, perform `ary[i] += v` and return the previous value of `ary[i]`.
+    Atomically, perform `ary[i] += v` and return the previous v of `ary[i]`.
 
-    i must be a simple index for a single element of ary. Broadcasting and vector operations are not supported.
-
-    This should be used from numba compiled code.
-    """
-def atomic_sub(ary, i, v):
-    """
-    Atomically, perform `ary[i] -= v` and return the previous value of `ary[i]`.
-
-    i must be a simple index for a single element of ary. Broadcasting and vector operations are not supported.
+    i must be a simple i for a single element of ary. Broadcasting and vector operations are not supported.
 
     This should be used from numba compiled code.
     """
-def atomic_max(ary, i, v):
+
+def atomic_sub(ary: NDArray[T], i: Union[int, tuple[int,...]], v: T) -> T:
     """
-    Atomically, perform `ary[i] = max(ary[i], v)` and return the previous value of `ary[i]`.
+    Atomically, perform `ary[i] -= v` and return the previous v of `ary[i]`.
+
+    i must be a simple i for a single element of ary. Broadcasting and vector operations are not supported.
+
+    This should be used from numba compiled code.
+    """
+
+def atomic_max(ary: NDArray[T], i: Union[int, tuple[int,...]], v: T) -> T:
+    """
+    Atomically, perform `ary[i] = max(ary[i], v)` and return the previous v of `ary[i]`.
     This operation does not support floating-point values.
 
-    i must be a simple index for a single element of ary. Broadcasting and vector operations are not supported.
+    i must be a simple i for a single element of ary. Broadcasting and vector operations are not supported.
 
     This should be used from numba compiled code.
     """
-def atomic_min(ary, i, v):
+
+def atomic_min(ary: NDArray[T], i: Union[int, tuple[int,...]], v: T) -> T:
     """
-    Atomically, perform `ary[i] = min(ary[i], v)` and return the previous value of `ary[i]`.
+    Atomically, perform `ary[i] = min(ary[i], v)` and return the previous v of `ary[i]`.
     This operation does not support floating-point values.
 
-    i must be a simple index for a single element of ary. Broadcasting and vector operations are not supported.
+    i must be a simple i for a single element of ary. Broadcasting and vector operations are not supported.
 
     This should be used from numba compiled code.
     """
-def atomic_xchg(ary, i, v): ...
+
+def atomic_xchg(ary: NDArray[T], i: Union[int, tuple[int,...]], v: T) -> T:
+    """
+    Atomically, perform `ary[i] = v` and return the previous v of `ary[i]`.
+
+    i must be a simple i for a single element of ary. Broadcasting and vector operations are not supported.
+
+    This should be used from numba compiled code.
+    """
--- stubs/numba_progress/progress.pyi
+++ stubs/numba_progress/progress.pyi
@@ -1,13 +1,22 @@
-from .numba_atomic import atomic_add as atomic_add, atomic_xchg as atomic_xchg
-from _typeshed import Incomplete
+from typing import Any, Optional, TextIO, TypeVar, Union, Dict
+from threading import Thread, Event
+from types import TracebackType
+import numpy as np
+from numpy.typing import NDArray
+import sys
+from tqdm import tqdm
+from tqdm.notebook import tqdm as tqdm_notebook
 from numba import types
-from numba.extending import lower_getattr as lower_getattr, lower_setattr as lower_setattr, models
+from numba.core.types.abstract import Type
+from numba.core.datamodel.new_models import StructModel
+from numba.extending import models
 
-def is_notebook():
-    """Determine if we're running within an IPython kernel
+from .numba_atomic import atomic_add, atomic_xchg
 
-    >>> is_notebook()
-    False
+def is_notebook() -> bool:
+    """Determine if we're running within an IPython kernel.
+
+    Returns whether the code is running in an IPython notebook environment.
     """
 
 class ProgressBar:
@@ -15,74 +24,100 @@ class ProgressBar:
     Wraps the tqdm progress bar enabling it to be updated from within a numba nopython function.
     It works by spawning a separate thread that updates the tqdm progress bar based on an atomic counter which can be
     accessed within the numba function. The progress bar works with parallel as well as sequential numba functions.
-    
-    Note: As this Class contains python objects not useable or convertable into numba, it will be boxed as a
+
+    Note: As this Class contains python objects not useable or convertible into numba, it will be boxed as a
     proxy object, that only exposes the minimum subset of functionality to update the progress bar. Attempts
     to return or create a ProgressBar within a numba function will result in an error.
 
     Parameters
     ----------
-    file: `io.TextIOWrapper` or `io.StringIO`, optional
-        Specifies where to output the progress messages
-        (default: sys.stdout). Uses `file.write(str)` and `file.flush()`
-        methods.  For encoding, see `write_bytes`.
+    file: TextIO, optional
+        Specifies where to output the progress messages (default: sys.stdout).
+        Uses `file.write(str)` and `file.flush()` methods.
+
     update_interval: float, optional
-        The interval in seconds used by the internal thread to check for updates [default: 0.1].
+        The interval in seconds used by the internal thread to check for updates (0.1).
+
     notebook: bool, optional
-        If set, forces or forbits the use of the notebook progress bar. By default the best progress bar will be
-        determined automatically.
+        If set, forces or forbids the use of the notebook progress bar.
+        By default the best progress bar will be determined automatically.
+
     dynamic_ncols: bool, optional
-        If true, the number of columns (the width of the progress bar) is constantly adjusted. This improves the
-        output of the notebook progress bar a lot.
+        If true, the number of columns (the width of the progress bar) is constantly adjusted.
+        This improves the output of the notebook progress bar a lot.
+
     kwargs: dict-like, optional
-        Addtional parameters passed to the tqdm class. See https://github.com/tqdm/tqdm for a documentation of
-        the available parameters. Noteable exceptions are the parameters:
-            - file is redefined above (see above)
-            - iterable is not available because it would not make sense here
-            - dynamic_ncols is defined above
+        Additional parameters passed to the tqdm class. See https://github.com/tqdm/tqdm
+        for a documentation of the available parameters.
     """
     _last_value: int
-    _tqdm: Incomplete
-    hook: Incomplete
-    _updater_thread: Incomplete
-    _exit_event: Incomplete
-    update_interval: Incomplete
-    def __init__(self, file: Incomplete | None = None, update_interval: float = 0.1, notebook: Incomplete | None = None, dynamic_ncols: bool = True, **kwargs) -> None: ...
-    _timer: Incomplete
+    _tqdm: tqdm[Any] | tqdm_notebook
+    hook: NDArray[np.uint64]
+    _updater_thread: Thread | None
+    _exit_event: Event
+    update_interval: float
+    _timer: Thread
+
+    def __init__(
+        self,
+        total: int | None = None,
+        file: TextIO | None = None,
+        update_interval: float = 0.1,
+        notebook: bool | None = None,
+        dynamic_ncols: bool = True,
+        **kwargs: Any
+    ) -> None: ...
+
     def _start(self) -> None: ...
+
     def close(self) -> None: ...
+
     @property
-    def n(self): ...
+    def n(self) -> int: ...
+
     def set(self, n: int = 0) -> None: ...
+
     def update(self, n: int = 1) -> None: ...
+
     def _update_tqdm(self) -> None: ...
+
     def _update_function(self) -> None:
         """Background thread for updating the progress bar.
         """
-    def __enter__(self): ...
-    def __exit__(self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: types.TracebackType | None) -> None: ...
 
-class ProgressBarTypeImpl(types.Type):
+    def __enter__(self) -> ProgressBar: ...
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None
+    ) -> None: ...
+
+class ProgressBarTypeImpl(Type):
+    """Type class for the Numba-compiled version of ProgressBar."""
     def __init__(self) -> None: ...
 
-ProgressBarType: Incomplete
+# This is the numba type representation of the ProgressBar class to be used in function signatures
+ProgressBarType: ProgressBarTypeImpl
 
-def typeof_index(val, c): ...
+class ProgressBarModel(StructModel):
+    """StructModel implementation for the ProgressBar in Numba."""
+    def __init__(self, dmm: Any, fe_type: Any) -> None: ...
 
-class ProgressBarModel(models.StructModel):
-    def __init__(self, dmm, fe_type) -> None: ...
+# The following functions are internal to Numba and normally not called directly
+def typeof_index(val: Any, c: Any) -> ProgressBarTypeImpl: ...
 
-def get_value(progress_bar): ...
-def unbox_progressbar(typ, obj, c):
-    """
-    Convert a ProgressBar to it's native representation (proxy object)
-    """
-def box_progressbar(typ, val, c) -> None: ...
-def _ol_update(self, n: int = 1):
-    """
-    Numpy implementation of the update method.
-    """
-def _ol_set(self, n: int = 0):
-    """
-    Numpy implementation of the update method.
-    """
+def get_value(progress_bar: ProgressBarTypeImpl) -> Any: ...
+
+def unbox_progressbar(typ: Any, obj: Any, c: Any) -> Any:
+    """Convert a ProgressBar to its native representation (proxy object)."""
+
+def box_progressbar(typ: Any, val: Any, c: Any) -> None:
+    """Cannot convert back to Python object since it contains internal Python state."""
+
+def _ol_update(self: ProgressBarTypeImpl, n: int = 1) -> Any:
+    """Numba implementation of the update method."""
+
+def _ol_set(self: ProgressBarTypeImpl, n: int = 0) -> Any:
+    """Numba implementation of the set method."""
