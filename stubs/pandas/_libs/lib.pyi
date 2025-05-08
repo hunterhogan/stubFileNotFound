f78bea5cf5686459078b57897d99dbd66e5f59e6
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-05-08 18:02:05 -0500

    pandas?

--- stubs/pandas/_libs/lib.pyi
+++ stubs/pandas/_libs/lib.pyi
@@ -1,81 +1,33 @@
 import _cython_3_0_11
 import enum
+import numpy as np
+from collections.abc import Callable
 from pandas._config import using_pyarrow_string_dtype as using_pyarrow_string_dtype
 from pandas._libs.interval import Interval as Interval
 from pandas._libs.missing import check_na_tuples_nonequal as check_na_tuples_nonequal
 from pandas._libs.tslibs.np_datetime import OutOfBoundsDatetime as OutOfBoundsDatetime, OutOfBoundsTimedelta as OutOfBoundsTimedelta
 from pandas._libs.tslibs.period import Period as Period
-from typing import ClassVar
+from pandas._typing import (
+    ArrayLike,
+    DtypeObj,
+)
+import numpy.typing as npt
+from decimal import Decimal
+from typing import (
+    Any,
+    ClassVar,
+    Final,
+    Generator,
+    Hashable,
+    Literal,
+    overload,
+    TypeAlias,
+    TypeGuard,
+)
+from enum import Enum
 
-from collections.abc import Callable
+ndarray_obj_2d = np.ndarray
 
-_TYPE_MAP: dict
-__pyx_capi__: dict
-__reduce_cython__: _cython_3_0_11.cython_function_or_method
-__setstate_cython__: _cython_3_0_11.cython_function_or_method
-__test__: dict
-_map_infer_mask: _cython_3_0_11.fused_cython_function
-array_equivalent_object: _cython_3_0_11.cython_function_or_method
-convert_nans_to_NA: _cython_3_0_11.fused_cython_function
-count_level_2d: _cython_3_0_11.cython_function_or_method
-dicts_to_array: _cython_3_0_11.cython_function_or_method
-dtypes_all_equal: _cython_3_0_11.cython_function_or_method
-ensure_string_array: _cython_3_0_11.cython_function_or_method
-eq_NA_compat: _cython_3_0_11.cython_function_or_method
-fast_multiget: _cython_3_0_11.cython_function_or_method
-fast_unique_multiple_list: _cython_3_0_11.cython_function_or_method
-fast_unique_multiple_list_gen: _cython_3_0_11.cython_function_or_method
-fast_zip: _cython_3_0_11.cython_function_or_method
-generate_bins_dt64: _cython_3_0_11.cython_function_or_method
-generate_slices: _cython_3_0_11.cython_function_or_method
-get_level_sorter: _cython_3_0_11.cython_function_or_method
-get_reverse_indexer: _cython_3_0_11.cython_function_or_method
-has_infs: _cython_3_0_11.fused_cython_function
-has_only_ints_or_nan: _cython_3_0_11.fused_cython_function
-i8max: int
-indices_fast: _cython_3_0_11.cython_function_or_method
-infer_dtype: _cython_3_0_11.cython_function_or_method
-is_all_arraylike: _cython_3_0_11.cython_function_or_method
-is_bool: _cython_3_0_11.cython_function_or_method
-is_bool_array: _cython_3_0_11.cython_function_or_method
-is_bool_list: _cython_3_0_11.cython_function_or_method
-is_complex: _cython_3_0_11.cython_function_or_method
-is_date_array: _cython_3_0_11.cython_function_or_method
-is_datetime64_array: _cython_3_0_11.cython_function_or_method
-is_datetime_array: _cython_3_0_11.cython_function_or_method
-is_datetime_with_singletz_array: _cython_3_0_11.cython_function_or_method
-is_decimal: _cython_3_0_11.cython_function_or_method
-is_float: _cython_3_0_11.cython_function_or_method
-is_float_array: _cython_3_0_11.cython_function_or_method
-is_int_or_none: _cython_3_0_11.cython_function_or_method
-is_integer: _cython_3_0_11.cython_function_or_method
-is_integer_array: _cython_3_0_11.cython_function_or_method
-is_interval: _cython_3_0_11.cython_function_or_method
-is_interval_array: _cython_3_0_11.cython_function_or_method
-is_iterator: _cython_3_0_11.cython_function_or_method
-is_list_like: _cython_3_0_11.cython_function_or_method
-is_np_dtype: _cython_3_0_11.cython_function_or_method
-is_period: _cython_3_0_11.cython_function_or_method
-is_pyarrow_array: _cython_3_0_11.cython_function_or_method
-is_range_indexer: _cython_3_0_11.fused_cython_function
-is_scalar: _cython_3_0_11.cython_function_or_method
-is_string_array: _cython_3_0_11.cython_function_or_method
-is_time_array: _cython_3_0_11.cython_function_or_method
-is_timedelta_or_timedelta64_array: _cython_3_0_11.cython_function_or_method
-item_from_zerodim: _cython_3_0_11.cython_function_or_method
-map_infer: _cython_3_0_11.cython_function_or_method
-map_infer_mask: _cython_3_0_11.cython_function_or_method
-maybe_booleans_to_slice: _cython_3_0_11.cython_function_or_method
-maybe_convert_numeric: _cython_3_0_11.cython_function_or_method
-maybe_convert_objects: _cython_3_0_11.cython_function_or_method
-maybe_indices_to_slice: _cython_3_0_11.cython_function_or_method
-memory_usage_of_objects: _cython_3_0_11.cython_function_or_method
-no_default: _NoDefault
-pa: None
-to_object_array: _cython_3_0_11.cython_function_or_method
-to_object_array_tuples: _cython_3_0_11.cython_function_or_method
-tuples_to_object_array: _cython_3_0_11.cython_function_or_method
-u8max: int
 
 class _NoDefault(enum.Enum):
     __new__: ClassVar[Callable] = ...
@@ -93,3 +45,189 @@ class _NoDefault(enum.Enum):
     @classmethod
     def _new_member_(cls, *args, **kwargs):
         """Create and return a new object.  See help(type) for accurate signature."""
+    no_default = ...
+
+no_default: Final = _NoDefault.no_default
+NoDefault: TypeAlias = Literal[_NoDefault.no_default]
+
+i8max: int
+u8max: int
+pa: None
+
+_TYPE_MAP: dict
+__pyx_capi__: dict
+__test__: dict
+def is_np_dtype(dtype: object, kinds: str | None = ...) -> TypeGuard[np.dtype]: ...
+def item_from_zerodim(val: object) -> object: ...
+def infer_dtype(value: object, skipna: bool = ...) -> str: ...
+def is_iterator(obj: object) -> bool: ...
+def is_scalar(val: object) -> bool: ...
+def is_list_like(obj: object, allow_sets: bool = ...) -> bool: ...
+def is_pyarrow_array(obj: object) -> bool: ...
+def is_period(val: object) -> TypeGuard[Period]: ...
+def is_interval(obj: object) -> TypeGuard[Interval]: ...
+def is_decimal(obj: object) -> TypeGuard[Decimal]: ...
+def is_complex(obj: object) -> TypeGuard[complex]: ...
+def is_bool(obj: object) -> TypeGuard[bool | np.bool_]: ...
+def is_integer(obj: object) -> TypeGuard[int | np.integer]: ...
+def is_int_or_none(obj) -> bool: ...
+def is_float(obj: object) -> TypeGuard[float]: ...
+def is_interval_array(values: np.ndarray) -> bool: ...
+def is_datetime64_array(values: np.ndarray, skipna: bool = True) -> bool: ...
+def is_timedelta_or_timedelta64_array(
+    values: np.ndarray, skipna: bool = True
+) -> bool: ...
+def is_datetime_with_singletz_array(values: np.ndarray) -> bool: ...
+def is_time_array(values: np.ndarray, skipna: bool = ...): ...
+def is_date_array(values: np.ndarray, skipna: bool = ...): ...
+def is_datetime_array(values: np.ndarray, skipna: bool = ...): ...
+def is_string_array(values: np.ndarray, skipna: bool = ...): ...
+def is_float_array(values: np.ndarray): ...
+def is_integer_array(values: np.ndarray, skipna: bool = ...): ...
+def is_bool_array(values: np.ndarray, skipna: bool = ...): ...
+def fast_multiget(
+    mapping: dict,
+    keys: np.ndarray,  # object[:]
+    default=...,
+) -> np.ndarray: ...
+def fast_unique_multiple_list_gen(gen: Generator, sort: bool = ...) -> list: ...
+def fast_unique_multiple_list(lists: list, sort: bool | None = ...) -> list: ...
+def map_infer(
+    arr: np.ndarray,
+    f: Callable[[Any], Any],
+    convert: bool = ...,
+    ignore_na: bool = ...,
+) -> np.ndarray: ...
+@overload
+def maybe_convert_objects(
+    objects: npt.NDArray[np.object_],
+    *,
+    try_float: bool = ...,
+    safe: bool = ...,
+    convert_numeric: bool = ...,
+    convert_non_numeric: Literal[False] = ...,
+    convert_to_nullable_dtype: Literal[False] = ...,
+    dtype_if_all_nat: DtypeObj | None = ...,
+) -> npt.NDArray[np.object_ | np.number]: ...
+@overload
+def maybe_convert_objects(
+    objects: npt.NDArray[np.object_],
+    *,
+    try_float: bool = ...,
+    safe: bool = ...,
+    convert_numeric: bool = ...,
+    convert_non_numeric: bool = ...,
+    convert_to_nullable_dtype: Literal[True] = ...,
+    dtype_if_all_nat: DtypeObj | None = ...,
+) -> ArrayLike: ...
+@overload
+def maybe_convert_objects(
+    objects: npt.NDArray[np.object_],
+    *,
+    try_float: bool = ...,
+    safe: bool = ...,
+    convert_numeric: bool = ...,
+    convert_non_numeric: bool = ...,
+    convert_to_nullable_dtype: bool = ...,
+    dtype_if_all_nat: DtypeObj | None = ...,
+) -> ArrayLike: ...
+@overload
+def maybe_convert_numeric(
+    values: npt.NDArray[np.object_],
+    na_values: set,
+    convert_empty: bool = ...,
+    coerce_numeric: bool = ...,
+    convert_to_masked_nullable: Literal[False] = ...,
+) -> tuple[np.ndarray, None]: ...
+@overload
+def maybe_convert_numeric(
+    values: npt.NDArray[np.object_],
+    na_values: set,
+    convert_empty: bool = ...,
+    coerce_numeric: bool = ...,
+    *,
+    convert_to_masked_nullable: Literal[True],
+) -> tuple[np.ndarray, np.ndarray]: ...
+
+# TODO: restrict `arr`?
+def ensure_string_array(
+    arr,
+    na_value: object = ...,
+    convert_na_value: bool = ...,
+    copy: bool = ...,
+    skipna: bool = ...,
+) -> npt.NDArray[np.object_]: ...
+def convert_nans_to_NA(
+    arr: npt.NDArray[np.object_],
+) -> npt.NDArray[np.object_]: ...
+def fast_zip(ndarrays: list) -> npt.NDArray[np.object_]: ...
+
+# TODO: can we be more specific about rows?
+def to_object_array_tuples(rows: object) -> ndarray_obj_2d: ...
+def tuples_to_object_array(
+    tuples: npt.NDArray[np.object_],
+) -> ndarray_obj_2d: ...
+
+# TODO: can we be more specific about rows?
+def to_object_array(rows: object, min_width: int = ...) -> ndarray_obj_2d: ...
+def dicts_to_array(dicts: list, columns: list) -> ndarray_obj_2d: ...
+def maybe_booleans_to_slice(
+    mask: npt.NDArray[np.uint8],
+) -> slice | npt.NDArray[np.uint8]: ...
+def maybe_indices_to_slice(
+    indices: npt.NDArray[np.intp],
+    max_len: int,
+) -> slice | npt.NDArray[np.intp]: ...
+def is_all_arraylike(obj: list) -> bool: ...
+
+# -----------------------------------------------------------------
+# Functions which in reality take memoryviews
+
+def memory_usage_of_objects(arr: np.ndarray) -> int: ...  # object[:]  # np.int64
+def map_infer_mask(
+    arr: np.ndarray,
+    f: Callable[[Any], Any],
+    mask: np.ndarray,  # const uint8_t[:]
+    convert: bool = ...,
+    na_value: Any = ...,
+    dtype: np.dtype = ...,
+) -> np.ndarray: ...
+def indices_fast(
+    index: npt.NDArray[np.intp],
+    labels: np.ndarray,  # const int64_t[:]
+    keys: list,
+    sorted_labels: list[npt.NDArray[np.int64]],
+) -> dict[Hashable, npt.NDArray[np.intp]]: ...
+def generate_slices(
+    labels: np.ndarray, ngroups: int  # const intp_t[:]
+) -> tuple[npt.NDArray[np.int64], npt.NDArray[np.int64]]: ...
+def count_level_2d(
+    mask: np.ndarray,  # ndarray[uint8_t, ndim=2, cast=True],
+    labels: np.ndarray,  # const intp_t[:]
+    max_bin: int,
+) -> np.ndarray: ...  # np.ndarray[np.int64, ndim=2]
+def get_level_sorter(
+    codes: np.ndarray,  # const int64_t[:]
+    starts: np.ndarray,  # const intp_t[:]
+) -> np.ndarray: ...  # np.ndarray[np.intp, ndim=1]
+def generate_bins_dt64(
+    values: npt.NDArray[np.int64],
+    binner: np.ndarray,  # const int64_t[:]
+    closed: object = ...,
+    hasnans: bool = ...,
+) -> np.ndarray: ...  # np.ndarray[np.int64, ndim=1]
+def array_equivalent_object(
+    left: npt.NDArray[np.object_],
+    right: npt.NDArray[np.object_],
+) -> bool: ...
+def has_infs(arr: np.ndarray) -> bool: ...  # const floating[:]
+def has_only_ints_or_nan(arr: np.ndarray) -> bool: ...  # const floating[:]
+def get_reverse_indexer(
+    indexer: np.ndarray,  # const intp_t[:]
+    length: int,
+) -> npt.NDArray[np.intp]: ...
+def is_bool_list(obj: list) -> bool: ...
+def dtypes_all_equal(types: list[DtypeObj]) -> bool: ...
+def is_range_indexer(
+    left: np.ndarray, n: int  # np.ndarray[np.int64, ndim=1]
+) -> bool: ...
