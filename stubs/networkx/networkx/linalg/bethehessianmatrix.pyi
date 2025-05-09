072b9e7fd9c0218616bc1266d64ad6dcb32bd899
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-05-09 14:49:29 -0500

    gluttony is a sin

--- stubs/networkx/networkx/linalg/bethehessianmatrix.pyi
+++
@@ -1,7 +0,0 @@
-from _typeshed import Incomplete
-from collections.abc import Collection
-
-from networkx.utils.backends import _dispatchable
-
-@_dispatchable
-def bethe_hessian_matrix(G, r: Incomplete | None = None, nodelist: Collection[Incomplete] | None = None): ...
