12c972f0070d21abb8072437a509dfc7931cf82c
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-06-28 00:11:37 -0500

    roboshed

--- roboshed.cmd
+++ roboshed.cmd
@@ -1,7 +1,13 @@
-robocopy typeshed\stdlib stubs\stdlib /s
-robocopy pandas-stubs\pandas-stubs stubs\pandas /s
+@ECHO OFF
+robocopy typeshed\stdlib stubs\stdlib /S /MT
+robocopy pandas-stubs\pandas-stubs stubs\pandas /S /MT
+
+FOR /F "tokens=1" %%G IN ('dir typeshed\stubs /b /AD') DO (
+    robocopy typeshed\stubs\%%G stubs /S /XD "@tests" "networkx" "six" /XF "METADATA.toml" /NFL /NDL /MT
+)
 
 IF NOT DEFINED VIRTUAL_ENV exit CALL .venv\Scripts\activate.bat
 
-cd stubs
+pushd stubs
 stubdefaulter --stdlib-path stdlib --packages .
+popd
