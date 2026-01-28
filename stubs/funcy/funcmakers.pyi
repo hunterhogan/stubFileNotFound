1d9c2fa2b43d40fb7980f10c6427d2780cb0775b
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-01-28 18:02:49 -0500

    `funcy` remove: I don't use it.

--- stubs/funcy/funcmakers.pyi
+++
@@ -1,8 +0,0 @@
-from collections.abc import Callable
-from operator import itemgetter
-from typing import Any
-
-__all__ = ['make_func', 'make_pred']
-
-def make_func(f: Any, test: bool = False) -> Callable[..., object] | type[bool] | Callable[..., Any] | Callable[..., bool] | Callable[..., Any | None] | itemgetter[int | slice[Any, Any, Any]] | Callable[[Any], Any]: ...
-def make_pred(pred: Any) -> Callable[..., object] | type[bool] | Callable[..., Any] | Callable[..., bool] | Callable[..., Any | None] | itemgetter[int | slice[Any, Any, Any]] | Callable[[Any], Any]: ...
