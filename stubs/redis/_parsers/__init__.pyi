3a7278871f5c114c927ec786d2a4ef40b7f196e9
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-02-08 18:10:16 -0500

    remove redis

--- stubs/redis/_parsers/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from .base import AsyncPushNotificationsParser as AsyncPushNotificationsParser, BaseParser as BaseParser, PushNotificationsParser as PushNotificationsParser, _AsyncRESPBase as _AsyncRESPBase
-from .commands import AsyncCommandsParser as AsyncCommandsParser, CommandsParser as CommandsParser
-from .encoders import Encoder as Encoder
-from .hiredis import _AsyncHiredisParser as _AsyncHiredisParser, _HiredisParser as _HiredisParser
-from .resp2 import _AsyncRESP2Parser as _AsyncRESP2Parser, _RESP2Parser as _RESP2Parser
-from .resp3 import _AsyncRESP3Parser as _AsyncRESP3Parser, _RESP3Parser as _RESP3Parser
-
-__all__ = ['AsyncCommandsParser', '_AsyncHiredisParser', '_AsyncRESPBase', '_AsyncRESP2Parser', '_AsyncRESP3Parser', 'AsyncPushNotificationsParser', 'CommandsParser', 'Encoder', 'BaseParser', '_HiredisParser', '_RESP2Parser', '_RESP3Parser', 'PushNotificationsParser']
--- stubs/redis/commands/json/_util.pyi
+++
@@ -1,3 +0,0 @@
-from typing import Any
-
-JsonType = str | int | float | bool | None | dict[str, Any] | list[Any]
